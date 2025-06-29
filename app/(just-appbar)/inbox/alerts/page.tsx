a9879d498e8917e74c25913409f24124a9c38a65
CourantLuna <courantluna@gmail.com>
2025-06-29 13:42:42 -0400

    a ver

--- app/(just-appbar)/inbox/alerts/page.tsx
+++ app/(just-appbar)/inbox/alerts/page.tsx
@@ -1,5 +1,7 @@
 // app/inbox/alerts/page.tsx
 "use client";
+export const dynamic = "force-dynamic";
+
 
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
