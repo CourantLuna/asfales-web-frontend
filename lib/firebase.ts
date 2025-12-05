789ac3a9ff41fe0df119cf810739b1bcdda5595c
courantluna <courantluna@gmail.com>
2025-12-05 04:33:57 -0400

    firebaseconfig configurado con env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,21 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXugoTSWw7gZYWLsvqLNdqaYIdIcdx9O8",
-  authDomain: "asfales-9f559.firebaseapp.com",
-  projectId: "asfales-9f559",
-  storageBucket: "asfales-9f559.firebasestorage.app",
-  messagingSenderId: "917627801076",
-  appId: "1:917627801076:web:ebf63b47fa266281a9fda1",
-  measurementId: "G-PR2FLXWCQE"
+ apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+console.log("firebaseFrontend",firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
